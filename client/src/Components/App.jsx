8de785d6a4ed1df10d58c43fe0bcde91fce2c3c1
Samuel Shih <samuels.shih@gmail.com>
2018-11-01 15:34:47 -0700

    initial set up

---
+++ client/src/Components/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <button type="button" class="btn btn-primary">
+          {' '}
+          Samuel Shih{' '}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
